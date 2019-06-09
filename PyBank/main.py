32546e781d631012cf1be4d19ed26d06efbe522d
Chris McCown <cmccown1@gmail.com>
2019-06-09 11:49:36 -0400

    worked on pybank challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,16 +1,21 @@
+
+# import my dependencies
 import os
 import csv
 
-budget_csv = os.path.join("budget_data.csv")
+# set my input file
+budget_csv = os.path.join('budget_data.csv')
 
-month = []
-net = []
-maxval = 0
-minval = 0
+#initialize variables
+month = []      # list to store month values in input file
+net = []        # list to store profit/loss values in unput file
+maxval = 0      # keep track of the maximum net value
+minval = 0      # keep track of the minimum net value
 
-with open(budget_csv,newline="",encoding='utf-8-sig') as csvfile:
-
-    csvreader = csv.reader(csvfile,delimiter=",")
+# using a with statement, create the file object to pass to the reader
+with open(budget_csv,newline='',encoding='utf-8-sig') as csvfile:
+    # create the reader object
+    csvreader = csv.reader(csvfile,delimiter=',')
     
     headers = next(csvreader)
 
@@ -18,6 +23,10 @@ with open(budget_csv,newline="",encoding='utf-8-sig') as csvfile:
             month.append(row[0])
             net.append(int(row[1]))
 
+
+# now begin the anlaysis
+
+# keep track of the index and the value
 for index,val in enumerate(net):
     if val>maxval:
         maxval = val
@@ -28,23 +37,22 @@ for index,val in enumerate(net):
 
 
 # :,.2f   :,f try these for formatting
-print("Financial Analysis")
-print("------------------------------")
-print(f"Total Months: {len(month)}")
-print(f"Total: ${sum(net):,.0f}")
-print(f"Average Change: ${sum(net)/len(net):,.2f}")
-print(f"Greatest Increase in profits: {month[maxind]} (${maxval:,.0f})")
-print(f"Greatest Decrease in profits: {month[minind]} (${minval:,.0f})")
+print('Financial Analysis')
+print('------------------------------')
+print(f'Total Months: {len(month)}')
+print(f'Total: ${sum(net):,.0f}')
+print(f'Average Change: ${sum(net)/len(net):,.2f}')
+print(f'Greatest Increase in profits: {month[maxind]} (${maxval:,.0f})')
+print(f'Greatest Decrease in profits: {month[minind]} (${minval:,.0f})')
 
+# using with statement, create the file object to pass to the writer
+with open('pybank.txt','w')  as file_out:
 
-file_out = open("pybank.txt","w") 
-
-file_out.write("Financial Analysis\n")
-file_out.write("------------------------------\n")
-file_out.write(f"Total Months: {len(month)}\n")
-file_out.write(f"Total: ${sum(net):,.0f}\n")
-file_out.write(f"Average Change: ${sum(net)/len(net):,.2f}\n")
-file_out.write(f"Greatest Increase in profits: {month[maxind]} (${maxval:,.0f})\n")
-file_out.write(f"Greatest Decrease in profits: {month[minind]} (${minval:,.0f})\n")
-
-file_out.close()
+    # write the analysis results, line by line
+    file_out.write('Financial Analysis\n')
+    file_out.write('------------------------------\n')
+    file_out.write(f'Total Months: {len(month)}\n')
+    file_out.write(f'Total: ${sum(net):,.0f}\n')
+    file_out.write(f'Average Change: ${sum(net)/len(net):,.2f}\n')
+    file_out.write(f'Greatest Increase in profits: {month[maxind]} (${maxval:,.0f})\n')
+    file_out.write(f'Greatest Decrease in profits: {month[minind]} (${minval:,.0f})\n')
